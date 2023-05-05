efebd7bcd6e5140e701d3dc66669c627404152c7
Sergii Zhuravskyi <szhuravskyi@nanthealth.com>
2023-05-05 17:59:53 +0100

    working project

--- JSONWebTokensJWTNet7NotMInimal/JSONWebTokensJWTNet7NotMInimal/Controllers/AuthController.cs
+++ JSONWebTokensJWTNet7NotMInimal/JSONWebTokensJWTNet7NotMInimal/Controllers/AuthController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JSONWebTokensJWTNet7NotMInimal.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace JSONWebTokensJWTNet7NotMInimal.Controllers
 {
@@ -7,5 +13,57 @@ namespace JSONWebTokensJWTNet7NotMInimal.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration  configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        [HttpPost("registered")]
+        public ActionResult<User> Register(UserDto request) {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (user.Username != request.Username) {
+                return BadRequest("User not found.");
+            }
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash)) {
+                return BadRequest("Wrong password");
+            }
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private string CreateToken(User user) {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.Username )
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddDays(1),
+                    signingCredentials : cred
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
     }
 }
